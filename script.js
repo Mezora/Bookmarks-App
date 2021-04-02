0667e792fdb4fa3b53ac54b38fec13942239eb34
Mezora <kalos.mezora@gmail.com>
2021-04-02 00:09:43 -0500

    Change color palette

--- script.js
+++ script.js
@@ -5,16 +5,65 @@ const bookmarkForm = document.getElementById('bookmark-form');
 const websiteNameEl = document.getElementById('website-name');
 const websiteUrlEl = document.getElementById('website-url');
 const bookmarksContainer = document.getElementById('bookmarks-container');
+// Palette 
+const paletteButton = document.getElementById('button-palette');
+const paletteModal = document.getElementById('modal-palette-wrapper');
+const paletteModalClose = document.getElementById('close-palette-modal');
+const paletteRed = document.getElementById('palette-red');
+const paletteBlue = document.getElementById('palette-blue');
+const palettePurple = document.getElementById('palette-purple');
+const paletteOrange= document.getElementById('palette-orange');
+const paletteGreen = document.getElementById('palette-green');
+const paletteDark = document.getElementById('palette-dark');
+
+const paletteColors = {
+    'red': '#ff2f2f',
+    'blue': '#3179ff',
+    'purple': '#854495',
+    'orange': '#ffb71c',
+    'green': '#21aa4e',
+    'dark': '#222',
+}
 let bookmarks = [];
+
+// Palette show menu 
+function showPalette() {
+    paletteModal.classList.add('show-modal-palette');
+}
+
+paletteButton.addEventListener('click', showPalette);
+
+function paletteColorSelector(color) {
+    document.documentElement.style.setProperty('--primary-color', color);
+}
+
+// Palette color event listener
+paletteRed.addEventListener('click', function(){paletteColorSelector(paletteColors.red)});
+paletteBlue.addEventListener('click', function(){paletteColorSelector(paletteColors.blue)});
+palettePurple.addEventListener('click', function(){paletteColorSelector(paletteColors.purple)});
+paletteOrange.addEventListener('click', function(){paletteColorSelector(paletteColors.orange)});
+paletteGreen.addEventListener('click', function(){paletteColorSelector(paletteColors.green)});
+paletteDark.addEventListener('click', function(){paletteColorSelector(paletteColors.dark)});
+
+// Close modal palette
+paletteModalClose.addEventListener('click', () => {
+    paletteModal.classList.remove('show-modal-palette');
+});
+window.addEventListener('click', (e) => {
+    (e.target === paletteModal) ? paletteModal.classList.remove('show-modal-palette') : false;
+})
+
 // Show Modal - Focus on Input
 function showModal() {
     modal.classList.add('show-modal');
     websiteNameEl.focus();
 }
+
 // Modal Event Listeners
 modalShow.addEventListener('click', showModal);
 modalClose.addEventListener('click', () => modal.classList.remove('show-modal'));
 window.addEventListener('click', (e) => (e.target === modal) ? modal.classList.remove('show-modal') : false );
+
 // Validate Form 
 function validate(nameValue, urlValue) {
     const expression = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/g;
@@ -30,6 +79,7 @@ function validate(nameValue, urlValue) {
     // Valid
     return true;
 }
+
 // Build Bookmarks DOM
 function buildBookmarks() {
     // Remove all bookmark elements
@@ -63,6 +113,7 @@ function buildBookmarks() {
         bookmarksContainer.appendChild(item);
     });
 }
+
 // Fetch Bookmarks
 function fetchBookmarks() {
     // Get bookmarks from localStorage if avalible
@@ -80,6 +131,7 @@ function fetchBookmarks() {
     }
     buildBookmarks();
 }
+
 // Delete Bookmark
 function deleteBookmark(url) {
     bookmarks.forEach((singleElement, index) => {
@@ -91,6 +143,7 @@ function deleteBookmark(url) {
     localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
     fetchBookmarks();
 };
+
 // Handle data from Form
 function storeBookmark(e) {
     e.preventDefault();
@@ -112,7 +165,9 @@ function storeBookmark(e) {
     bookmarkForm.reset();
     websiteUrlEl.focus();
 }
+
 // Event Listener 
 bookmarkForm.addEventListener('submit', storeBookmark);
+
 // On Load, Fetch Bookmarks
 fetchBookmarks();
